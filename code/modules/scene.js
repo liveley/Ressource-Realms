0ee94274dbf1ab1dc6e572062e3941e053069eff
Moritz Finkel <m.finkel@hm.edu>
2025-05-22 13:20:25 +0200

    added lightning position and created an aquamarine background

--- code/modules/scene.js
+++ code/modules/scene.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xf0f0f0);
+scene.background = new THREE.Color(0x2da7c1);
 
 export { scene };
