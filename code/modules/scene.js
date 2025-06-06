7d7fb1e1ff52f66ea23744f31e3d654f780fd3cc
Moritz Finkel <m.finkel@hm.edu>
2025-06-06 22:05:34 +0200

    Add skybox

--- code/modules/scene.js
+++ code/modules/scene.js
@@ -1,6 +1,17 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x8fd6f5); // Helleres, freundliches Blau für Wasserhintergrund
+
+// Skybox-Texturen laden
+const loader = new THREE.CubeTextureLoader();
+const skybox = loader.load([
+  './textures/skybox_px.jpg', // posX
+  './textures/skybox_nx.jpg', // negX
+  './textures/skybox_py.jpg', // posY
+  './textures/skybox_ny.jpg', // negY
+  './textures/skybox_pz.jpg', // posZ
+  './textures/skybox_nz.jpg'  // negZ
+]);
+scene.background = skybox;
 
 export { scene };
