120b846d6f69741e19f20476368226e3b6c4c185
Moritz Finkel <m.finkel@hm.edu>
2025-06-08 15:48:37 +0200

    Design better build UI

--- code/modules/uiBuild.js
+++ code/modules/uiBuild.js
@@ -5,26 +5,12 @@
 export function createBuildUI({ players, getBuildMode, setBuildMode, getActivePlayerIdx, setActivePlayerIdx }) {
   const ui = document.createElement('div');
   ui.id = 'build-ui';
-  ui.style.position = 'absolute';
-  ui.style.bottom = '2em';
-  ui.style.left = '50%';
-  ui.style.transform = 'translateX(-50%)';
-  ui.style.background = 'rgba(255,255,255,0.95)';
-  ui.style.borderRadius = '10px';
-  ui.style.padding = '10px 24px';
-  ui.style.boxShadow = '0 2px 8px #0002';
-  ui.style.display = 'flex';
-  ui.style.gap = '1.5em';
-  ui.style.fontFamily = "'Montserrat', Arial, sans-serif";
-  ui.style.fontSize = '1.2em';
-  ui.style.alignItems = 'center';
-  ui.style.zIndex = '20';
   ui.innerHTML = `
     <button id="build-settlement">Siedlung bauen</button>
     <button id="build-city">Stadt bauen</button>
     <button id="build-road">Straße bauen</button>
     <span>Aktiver Spieler: <select id="player-select"></select></span>
-    <span id="build-feedback" style="color:#d7263d;margin-left:1em;min-width:7em;min-height:1.5em;display:inline-block;vertical-align:middle;"></span>
+    <span id="build-feedback"></span>
   `;
   document.body.appendChild(ui);
   document.getElementById('build-settlement').onclick = () => setBuildMode('settlement');
